b4d7e6c1dd273d4366472c30c98de50fe099391b
solaarTW <jhedahl@hotmail.com>
2022-11-09 01:11:24 -0800

    Added export of change file but import function not implemented

--- DeepAssetDiscovery.py
+++ DeepAssetDiscovery.py
@@ -3,8 +3,6 @@ from tkinter import *
 from tkinter.ttk import *
 # Folder dialog compatible with Windows, Unix, and macOS
 from tkinter.filedialog import askdirectory
-# Measure screen width to resolve tkwindow
-import tkinter.font as tkfont
 # File handling and cancel buttons
 import os
 import sys
@@ -14,42 +12,22 @@ import subprocess
 import json
 # Recursion library
 from pathlib import Path
-# Use append to manage duplicate keys
-from collections import defaultdict
-# Optionally sort dictionary
-from operator import itemgetter
+from tkinter import ttk
 
 
 # Create UI for runtime settings and then pass control off to the code
 def CreateTkWindow():
-    # Calculate resolution of screen for dynamic resizing
-    font = tkfont.Font(family="Consolas", size=10, weight="normal")
-    width = 0
-    height = 1
-    # Identify the quantity of characters to fill the largest width and height
-    for key, val in ScriptParameters['SearchModes'].items():
-        if len(key+val) > width:
-            width = len(key+val)
-        else:
-            None # housekeeping
-        height += 1
-    # Uneducated calculations for the dynamic resizing
-    width = width * font.measure("m")
-    height = round(100/(height*2)) * font.measure("m") * len(ScriptParameters['SearchModes'])
-    # Hopes and prayers
-    tkwindow.geometry(f"{width}x{height}")
     # Tkinter string variable that will anchor the radiobutton selections
-    ScriptParameters['SearchMode'] = StringVar(tkwindow, "Include")
-    # ScriptParameters['SortMode'] = StringVar(tkwindow, "Chronological")
+    ScriptParameters['RunMode'] = StringVar(tkwindow, 'Discovery')
     header = StringVar()
     # Create radiobuttons for all the search modes
-    label = Label( tkwindow, textvariable=header, font= ('Helvetica 15 underline')).pack(anchor=W)
-    header.set("Choose what keys to filter for:")
-    for (searchmode, description) in ScriptParameters['SearchModes'].items():
-        Radiobutton(tkwindow, text = description, variable = ScriptParameters['SearchMode'],
-            value = searchmode).pack(side = TOP, anchor=W, ipady = 5)
-    # Checkbutton(tkwindow, text='Sort output alphabetically', variable=ScriptParameters['SortMode'], onvalue='Alphabetic', offvalue='Chronological').pack()
-    Button(tkwindow, text= "Set wkit project's raw folder", command=onClick).pack(pady= 10, anchor=W)
+    Label( tkwindow, textvariable=header, font= ('Helvetica 15 underline')).pack(anchor=W)
+    header.set('Deep Asset Discovery Tool:')
+    for (runmode, description) in ScriptParameters['RunModes'].items():
+        Radiobutton(tkwindow, text = description, variable = ScriptParameters['RunMode'], value = runmode
+                    ).pack(side = TOP, anchor=W, ipady = 0)
+    Button(tkwindow, text= "Set wkit project's raw folder", command=onClick
+           ).pack(pady= 5, anchor=W)
     # Infinite loop terminated in the onClick definition
     tkwindow.mainloop()
 
@@ -57,20 +35,20 @@ def CreateTkWindow():
 # tkinter button action to start the recursion and then stop the program when the parsing is done.
 # I have no idea why, but this program ends at this definition.
 def onClick():
-    RunMode(ScriptParameters['SearchMode'].get())
+    RunMode(ScriptParameters['RunMode'].get())
     DumpBuffer()
     open_file(ScriptParameters['ProjectDirectory'])
-    tkwindow.destroy()
+    sys.exit() 
 
 
 # Open file explorer at the dump file
 def open_file(path):
-    if platform.system() == "Windows":
+    if platform.system() == 'Windows':
         os.startfile(path)
-    elif platform.system() == "Darwin":
-        subprocess.Popen(["open", path])
+    elif platform.system() == 'Darwin':
+        subprocess.Popen(['open', path])
     else:
-        subprocess.Popen(["xdg-open", path])
+        subprocess.Popen(['xdg-open', path])
 
 
 # Convert file to JSON format
@@ -85,49 +63,54 @@ def convertJSON(tup):
 
 
 # Evaluate project files and initiate recursion
-# Input: ScriptParameters['SearchModes']
+# Input: ScriptParameters['RunModes']
 # Output: None (except the dummy light I guess)
 def RunMode(mode):
-    tkwindow.minsize()
-
     # Folder dialog prompt
     ScriptParameters['ProjectDirectory'] = askdirectory(title='Select Folder', initialdir='.') # shows dialog box and return the path
-    # Assign path to the output file
-    ScriptParameters['OutputFile'] = f"{ScriptParameters['ProjectDirectory']}/{ScriptParameters['OutputFile']}"
+    # Close program if cancel is clicked
+    sys.exit() if ScriptParameters['ProjectDirectory'] == '' else tkwindow.withdraw()
+
+    # Assign path to the output files
+    ScriptParameters['DiscoveryFile'] = f"{ScriptParameters['ProjectDirectory']}/{ScriptParameters['DiscoveryFile']}"
+    ScriptParameters['ChangeFile'] = f"{ScriptParameters['ProjectDirectory']}/{ScriptParameters['ChangeFile']}"
 
     # Delete file from previous runtime
-    if (os.path.isfile(ScriptParameters['OutputFile'])):
-        os.remove(ScriptParameters['OutputFile'])
+    if mode == 'Change':
+        data[1] = open(ScriptParameters['ChangeFile'], 'r', encoding='UTF-8', errors='ignore')
     else:
-        None # housekeeping
-    
+        if os.path.isfile(ScriptParameters['DiscoveryFile']):
+            os.remove(ScriptParameters['DiscoveryFile'])
+        else:
+            None # housekeeping
+        if (os.path.isfile(ScriptParameters['ChangeFile'])):
+            os.remove(ScriptParameters['ChangeFile'])
+        else:
+            None # housekeeping
+
+
     # Identify JSON files in the folder structure
-    RawFiles = list(Path(ScriptParameters['ProjectDirectory']).rglob("*.json"))
+    RawFiles = list(Path(ScriptParameters['ProjectDirectory']).rglob('*.json'))
     # Start recursion
     FoundBuffer['NumberOfFiles'] = len(RawFiles) - 1
     for FoundBuffer['cntIndex'], RawFile in enumerate(RawFiles):
         # Prepare the raw file for parsing
-        data = open(RawFile, 'r', encoding="UTF-8", errors="ignore")
+        if mode == 'Change':
+            data[0] = open(RawFile, 'w', encoding='UTF-8', errors='ignore')
+        else:
+            data = open(RawFile, 'r', encoding='UTF-8', errors='ignore')
         data = convertJSON(data)
         if isinstance(data, dict):
             FoundBuffer['Data'][RawFile.name] = dict()
-            ScriptParameters['InputFile'] = RawFile.name
-            data['EndOfFile'] = 'EndOfFile'
+            FoundBuffer['RawFile'] = RawFile.name
             RecursiveSearch(val=data, key=None)
         else:
             print(f"Could not convert {RawFile.name}")
     # Dummy light
-    print("Done")
+    print('Done')
 
 
 # Recursively drill down into JSON for specific key-value pairs
-# Input 1: Primary constaint to evaluate,
-# Input 2: Optional secondary holds meta info on primary constraint
-# Input 3: Initiates buffer to hold values from primary constraint
-# Output: None
-# Step 1: Identify type(primary)
-# Step 2: Recurse if possible or otherwise evaluate for FoundBuffer[]
-# Step 3: Log primary value or dump FoundBuffer[] to file
 def RecursiveSearch(val, key=None):
     # Dictory found -- dive into ScriptParameters
     if isinstance(val, dict):
@@ -140,41 +123,12 @@ def RecursiveSearch(val, key=None):
     # String found -- validate and then record if valid
     elif isinstance(val, str):
         # Ignore hashes
-        if (len(key) + len(val) < 200):
-            # Include search mode
-            if ScriptParameters['SearchMode'].get() == 'Include':
-                # Filter for keys in the Include list
-                if key.lower() in ScriptParameters['Include']:
-                    BufferData(key, val)
-                else:
-                    None # housekeeping
-            # Exclude search mode
-            elif ScriptParameters['SearchMode'].get() == 'Exclude':
-                # Filter out keys in the Exclude list
-                if key.lower() not in ScriptParameters['Exclude']:
-                    BufferData(key, val)
-                else:
-                    None # housekeeping
-            # Neither search mode
-            elif ScriptParameters['SearchMode'].get() == 'Neither':
-                # Filter out keys in Include and Exclude lists
-                if key.lower() not in [*ScriptParameters['Include'],*ScriptParameters['Exclude']]:
-                    BufferData(key, val)
-                else:
-                    None # housekeeping
-            # Discovery search mode
-            elif ScriptParameters['SearchMode'].get() == 'Discovery':
-                # Filter out keys in the Include list
-                if val.lower() not in ScriptParameters['Include']:
-                    BufferData(key, val)
-                else:
-                    None # housekeeping
-            # Oohhhhh yeeaaaaahhhhhhhhhhh
-            elif ScriptParameters['SearchMode'].get() == 'NoFilter':
+        if len(key) + len(val) < 200:
+            # Filter for keys in the Dsicover list
+            if key.lower() in ScriptParameters['Discovery']:
                 BufferData(key, val)
-            # Unexpected error
             else:
-                print(f"An Unexpected error occurred file {ScriptParameters['InputFile']} at {key}:::{val}\n")
+                None # housekeeper
         else:
             None # housekeeping
     # Integer, float, or null found -- ignore
@@ -182,15 +136,15 @@ def RecursiveSearch(val, key=None):
         None # I dunno, future development?
     # Unexpected error -- print for prosterity
     else:
-        if len(val) + len(key) < 100:
-            print(f"An Unexpected error occurred at {ScriptParameters['InputFile']}:::{Secondary}:::{Primary}\n")
+        if len(val) + len(key) < 200:
+            print(f"An Unexpected error occurred at {FoundBuffer['RawFile']}:::{key}:::{val}\n")
         else:
             None # housekeeping
 
 
 def BufferData(key, val):   
     # Shorthand the variable for easier reading below
-    file = ScriptParameters['InputFile']
+    file = FoundBuffer['RawFile']
     # Compile a list of linked files
     if key == 'DepotPath' and val not in FoundBuffer['MissingFiles']:
         FoundBuffer['MissingFiles'].append(val)
@@ -217,10 +171,10 @@ def RemoveArchiveFiles():
     # Replace ./raw with ./archive
     ArchiveLocation = (f"{str(ScriptParameters['ProjectDirectory'])[:-3]}archive")
     # Get all the archive files
-    ArchiveFiles = str(list(Path(ArchiveLocation).rglob("*.*")))
+    ArchiveFiles = str(list(Path(ArchiveLocation).rglob('*.*')))
     for ArchiveFile in ArchiveFiles:
         # Substring file's path to get /base...
-        ArchiveFile = ArchiveFile[ArchiveFile.find("base"):-5]
+        ArchiveFile = ArchiveFile[ArchiveFile.find('base'):-5]
         if ArchiveFile in FoundBuffer['MissingFiles']:
             # Remove alreaady added file from the missing list
             FoundBuffer['MissingFiles'].remove(ArchiveFile)
@@ -235,6 +189,28 @@ def sort_dict(item: dict):
     return {k: sort_dict(v) if isinstance(v, dict) else v for k, v in sorted(item.items())}
 
 
+# Return collapsed buffer for the file change_DAD.json
+def CollapseBuffer(item: dict):
+    for key, val in item.items():
+        if isinstance(val, dict):
+            CollapseBuffer(val)
+        elif isinstance(val, list):
+            for v in val:
+                if key not in FoundBuffer['CollapsedData']:
+                    FoundBuffer['CollapsedData'][key] = dict()
+                if (key, {v: ""}) not in FoundBuffer['CollapsedData'].items():
+                    FoundBuffer['CollapsedData'][key][v] = ""
+                else:
+                    None # housekeeping
+        else:
+            if key not in FoundBuffer['CollapsedData']:
+                FoundBuffer['CollapsedData'[key]] = dict()
+            if (key, {v: ""}) not in FoundBuffer['CollapsedData'].items():
+                FoundBuffer['CollapsedData'][key][v] = ""
+            else:
+                None # housekeeping
+
+
 # Dump discovery buffer to file in JSON format
 def DumpBuffer():
     # Identify and sort files missing from the project
@@ -243,73 +219,49 @@ def DumpBuffer():
     FoundBuffer['Data'] = dict(sort_dict(FoundBuffer['Data']))
     # Sort the missing files
     FoundBuffer['MissingFiles'] = sorted(FoundBuffer['MissingFiles'])
+    # Dump buffer to change file
+    CollapseBuffer(FoundBuffer['Data'])
+    # Sort buffer and convert to dict for json.dump
+    FoundBuffer['CollapsedData'] = dict(sort_dict(FoundBuffer['CollapsedData']))
+    with open(ScriptParameters['ChangeFile'], 'w', encoding='UTF-8') as output_c:
+        json.dump(FoundBuffer['CollapsedData'], output_c, ensure_ascii=False, indent=4)
+    FoundBuffer.pop('CollapsedData', None)
     # Move the missing files into the export section of the buffer
     FoundBuffer['Data']['Missing_Files'] = FoundBuffer['MissingFiles']
-    # Dump buffer to file
-    with open(ScriptParameters['OutputFile'], 'w', encoding='UTF-8') as output:
-        json.dump(FoundBuffer['Data'], output, ensure_ascii=False, indent=4)
-    output.close()
-    FoundBuffer['Data'] = dict() # Reset buffer
+    # Dump buffer to discovery file
+    with open(ScriptParameters['DiscoveryFile'], 'w', encoding='UTF-8') as output_d:
+        json.dump(FoundBuffer['Data'], output_d, ensure_ascii=False, indent=4)
+    output_d.close()
+    output_c.close()
 
 
 # Declare buffer queue of found keys during the recursive definition
 FoundBuffer = {
     'cntIndex': 0,
     'NumberOfFiles': 0,
+    'RawFile': '',
     'Data': dict(),
-    'MissingFiles':[]
+    'MissingFiles':[],
+    'CollapsedData': dict()
 }
 
 
-# Array of keywords to filter the recursion on
-# SearchMode == Include ::: Find keys that are in this list
-# SearchMode == Exclude ::: Ignore keys that are in this list
-# SearchMode == Neither ::: Ignore keys that are in both lists
-# SearchMode == Discovery ::: Ignore keys in the Include list
-# SearchMode == NoFilter ::: You know how I like it. Just load me up and crash this computer
-# SortMode == Chronological ::: Leave values as they were found in the files
-# SortMode == Alphabetic ::: Sort FoundBuffer['Data'] and then each individual list aphabetically
+# Outer wrapper of configuration
 ScriptParameters = {
     'ProjectDirectory': '',
-    'InputFile': '',
-    'OutputFile': 'out_DAD.json',
-    'SearchMode': 'Include',
-    'SearchModes': {
-        "Include": "Only find linking properties",
-        "Discovery": "Discovery mode that ignores known linking properties"
-        # "Exclude": "Find all keys except ones in the Exclude list",
-        # "Neither": "Find all keys except ones in the Include and Exclude list",
-        # "NoFilter": "Do not filter, Find all unique key-value pairs"
+    'ChangeFile': 'change_DAD.json',
+    'DiscoveryFile': 'discovery_DAD.json',
+    'RunMode': 'Discovery',
+    'RunModes': {
+        'Discovery': 'Discover linked values in JSON files.',
+        'Change': 'Import change_DAD.json to update your assets.'
         },
-    'SortMode': 'Chronological',
-    'Include': [
+    'Discovery': [
         'appearancename','appearanceresource',
         'componentname',
         'defaultAppearance','depotpath',
         'meshappearance',
         'name'
-    ],
-    'Exclude': [
-        '$type',
-        'activationstate',
-        'bodypart','bonename','bonenames','buffer','bufferid','bufferrefid','bytes',
-        'callbackaction','clnumber','cltimestamp','colorscale','compression','constrainttype','cookingplatform',
-        'datatype','density','devicestate','documenttype','EndOfFile','entryphasename','enumeratedtype','exporteddatetime',
-        'fieldname','flatfeetgroupname','forceopendocumenttype',
-        'gradientmode',
-        'handleid','hingeaxis',
-        'isoscriptcode',
-        'languagecode','languageid','liftedfeetgroupname','linktype','lootstate',
-        'mergemode','metallevelsin','metallevelsout','movexaxis','moveyaxis',
-        'normalstrength',
-        'onchildfailure','onchildsuccess',
-        'parentweightmode','pe','periodname','perspective','projectiontype',
-        'rootresolution','roughlevelsin','roughlevelsout',
-        'streamtype','submitkey','swingy','swingz',
-        'textdirection','timestampargument','twist','type',
-        'vectorcoodrinatetype',
-        'wkitjsonversion','wolvenkitversion',
-        'x','y','z'
     ]
 }
 
